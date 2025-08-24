b9735503a638b513ea7f858fa2a81e004ed86681
Jessica Alder <jessalder1989@gmail.com>
2025-08-24 15:26:29 -0700

    Updated project to raylib-quickstart for easy multi-platform support.

---
+++ DapperDasher/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <raylib.h>
+#include <raymath.h>
+#include "view.h"
+#include "character.h"
+#include "character_controller.h"
+#include "resource_dir.h"
+
+int main()
+{
+    // Initial window viewport setup. Check View for scaling
+    View Viewport;                          
+    InitWindow(Viewport.Width, Viewport.Height, "Dapper Dasher!");
+	SearchAndSetResourceDir("resources");
+
+    Character character(&Viewport);
+    CharacterController characterController(&character);
+
+    while (!WindowShouldClose()) {
+        Viewport.deltaTime = GetFrameTime();
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        characterController.UpdateMovement(&Viewport);
+        
+        EndDrawing();
+    }
+    
+    UnloadTexture(character.texture);
+    CloseWindow();
+    return 0;
+}
